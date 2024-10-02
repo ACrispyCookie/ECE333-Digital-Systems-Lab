d6838c0b7696ffefa4b07cf01e00d659497cfcc3
nikas <nikasjason1@gmail.com>
2024-10-02 14:32:17 +0300

    Add ECE333 and complete phase 1 of Lab 1

---
+++ Lab1/src/LEDdecoder.v
@@ -0,0 +1,44 @@
+module LEDdecoder(char, LED);
+    
+    input [5:0] char;
+    output reg [6:0] LED;
+
+    always @(char) begin
+        case (char)
+            6'd0: LED = 7'b1111110; // char '0' or 'O'
+            6'd1: LED = 7'b0110000; // char '1'
+            6'd2: LED = 7'b1101101; // char '2' or 'Z'
+            6'd3: LED = 7'b1111001; // char '3'
+            6'd4: LED = 7'b0110011; // char '4' 
+            6'd5: LED = 7'b1011011; // char '5' or 'S'
+            6'd6: LED = 7'b1011111; // char '6'
+            6'd7: LED = 7'b1110000; // char '7'
+            6'd8: LED = 7'b1111111; // char '8' or 'B'
+            6'd9: LED = 7'b1111011; // char '9'
+            6'd10: LED = 7'b0000000; // char ' '
+            6'd11: LED = 7'b1110111; // char 'A'
+            6'd12: LED = 7'b1001110; // char 'C'
+            6'd13: LED = 7'b1111100; // char 'D'
+            6'd14: LED = 7'b1001111; // char 'E'
+            6'd15: LED = 7'b1000111; // char 'F'
+            6'd16: LED = 7'b1011110; // char 'G'
+            6'd17: LED = 7'b0110111; // char 'H'
+            6'd18: LED = 7'b0000110; // char 'I'
+            6'd19: LED = 7'b0111100; // char 'J'
+            6'd20: LED = 7'b1010111; // char 'K'
+            6'd21: LED = 7'b0001110; // char 'L'
+            6'd22: LED = 7'b1101010; // char 'M'
+            6'd23: LED = 7'b1110110; // char 'N'
+            6'd24: LED = 7'b1100111; // char 'P'
+            6'd25: LED = 7'b1101011; // char 'Q'
+            6'd26: LED = 7'b1101111; // char 'R'
+            6'd27: LED = 7'b1000110; // char 'T'
+            6'd28: LED = 7'b0111110; // char 'U'
+            6'd29: LED = 7'b0111010; // char 'V'
+            6'd30: LED = 7'b1011100; // char 'W'
+            6'd31: LED = 7'b1001001; // char 'X'
+            6'd32: LED = 7'b0101011; // char 'Y'
+            default: LED = 7'b0000000;
+        endcase
+    end
+endmodule
---
+++ Lab1/src/LEDdecoder_testbench.v
@@ -0,0 +1,60 @@
+module LEDdecoder_testbench;
+    reg [5:0] char;
+    wire [6:0] LED;
+    wire CA = LED[6], CB = LED[5], CC = LED[4], CD = LED[3], CE = LED[2], CF = LED[1], CG = LED[0];
+
+
+    LEDdecoder decoder(char, LED);
+    wire [615:0] display = {" ", CA ? "aaaa" : "    ", " \n", 
+    CF ? "f" : " ", "    ", CB ? "b" : " ", "\n",
+    CF ? "f" : " ", "    ", CB ? "b" : " ", "\n",
+    CF ? "f" : " ", "    ", CB ? "b" : " ", "\n",
+    CF ? "f" : " ", "    ", CB ? "b" : " ", "\n",
+    " ", CG ? "gggg" : "    ", " \n",
+    CE ? "e" : " ", "    ", CC ? "c" : " ", "\n",
+    CE ? "e" : " ", "    ", CC ? "c" : " ", "\n",
+    CE ? "e" : " ", "    ", CC ? "c" : " ", "\n",
+    CE ? "e" : " ", "    ", CC ? "c" : " ", "\n",
+    " ", CD ? "dddd" : "    ", " \n"};
+
+    initial begin
+        $dumpfile("LEDdecoder_testbench.vcd");
+        $dumpvars(0, LEDdecoder_testbench);
+        $monitor("char = %d, LED = %b, display = \n%s", char, LED, display);
+        char = 6'd0;
+        #10 char = 6'd1;
+        #10 char = 6'd2;
+        #10 char = 6'd3;
+        #10 char = 6'd4;
+        #10 char = 6'd5;
+        #10 char = 6'd6;
+        #10 char = 6'd7;
+        #10 char = 6'd8;
+        #10 char = 6'd9;
+        #10 char = 6'd10;
+        #10 char = 6'd11;
+        #10 char = 6'd12;
+        #10 char = 6'd13;
+        #10 char = 6'd14;
+        #10 char = 6'd15;
+        #10 char = 6'd16;
+        #10 char = 6'd17;
+        #10 char = 6'd18;
+        #10 char = 6'd19;
+        #10 char = 6'd20;
+        #10 char = 6'd21;
+        #10 char = 6'd22;
+        #10 char = 6'd23;
+        #10 char = 6'd24;
+        #10 char = 6'd25;
+        #10 char = 6'd26;
+        #10 char = 6'd27;
+        #10 char = 6'd28;
+        #10 char = 6'd29;
+        #10 char = 6'd30;
+        #10 char = 6'd31;
+        #10 char = 6'd32;
+    end
+
+
+endmodule
